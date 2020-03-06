2ed6a9a9c6f541f5215d6f5aed59485a0b37bc96
Prateek Somaiya <prateek.somaiya@gmail.com>
2020-03-06 14:03:56 -0600

    Initial Commit
    Initial Commit

---
+++ src/main/java/com/psomaiya/aws/kinesis/producer/sample/SampleProducer.java
@@ -0,0 +1,11 @@
+package com.psomaiya.aws.kinesis.producer.sample;
+
+public class SampleProducer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello");
+	}
+
+}
